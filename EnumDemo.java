0d68438ae783b800462f6164ccb07acd860e2205
Prathin <59028729+prathin-rajesh@users.noreply.github.com>
2025-02-03 20:36:39 +0530

    using enum for constants

---
+++ EnumDemo.java
@@ -0,0 +1,15 @@
+enum DaysOfWeek {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
+
+public class EnumDemo {
+    public static void main(String[] args) {
+        DaysOfWeek day = DaysOfWeek.SUNDAY;
+
+        if (day == DaysOfWeek.SUNDAY || day == DaysOfWeek.SATURDAY) {
+            System.out.println("Holiday");
+        } else {
+            System.out.println("Not Holiday");
+        }
+    }
+}
