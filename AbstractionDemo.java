e1f4ec28d47966a1cc6512727a7ce517f51d74a5
Prathin <59028729+prathin-rajesh@users.noreply.github.com>
2025-02-03 20:28:24 +0530

    implementation  of abstract concept

---
+++ AbstractionDemo.java
@@ -0,0 +1,17 @@
+abstract class Animal {
+    public abstract void makeSound();
+}
+
+class Dog extends Animal {
+    @Override  //annotation is used to let the devs identify that overriding is happening 
+    public void makeSound() {
+        System.out.println("Dog is barking.");
+    }
+}
+
+public class AbstractionDemo {
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        myDog.makeSound();
+    }
+}
